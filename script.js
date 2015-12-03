c09874c26949d246aad9190666b72f67a1dcaa15
Jelle Sjollema <j.sjollema@ma-web.nl>
2015-12-03 11:23:59 +0100

    met twee punten

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ window.addEventListener("load",function(){
 	var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     
+    
+    var mouse = utils.captureMouse(canvas);
+    var A = new Point(100,100,20,"#ff0000",mouse);
+    var B = new Point(700,200,20,"#0000ff",mouse);
+    
+    
     (function animateCanvas(){
+        context.clearRect(0,0,800,450); 
         window.requestAnimationFrame(animateCanvas);
+        A.update();
+        A.draw(context);
+        B.update();
+        B.draw(context);
     })()
 });
 
