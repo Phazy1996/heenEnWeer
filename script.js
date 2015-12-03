3ee3d283d74f0f2d630c12111efc7c5effc6f293
Jelle Sjollema <j.sjollema@ma-web.nl>
2015-12-03 11:16:33 +0100

    met utils.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.addEventListener("load",function(){ 
 	var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
+    
+    (function animateCanvas(){
+        window.requestAnimationFrame(animateCanvas);
+    })()
 });
 
 
